5a6ea269a555f569388ce536405dad2abe40b828
sansx <xiathxth@gmail.com>
2019-07-06 22:30:50 +0800

    :construction: add scroll fn

--- src/components/TopList.vue
+++ src/components/TopList.vue
@@ -1,48 +1,28 @@
 <template>
   <v-layout row>
     <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-list two-line>
-          <template v-for="(item, index) in items">
-            <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
-
-            <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
-
-            <v-list-tile v-else :key="item.title" avatar @click="titleClick(item.title)">
-              <v-list-tile-avatar>
-                <img :src="item.avatar" />
-              </v-list-tile-avatar>
-
-              <v-list-tile-content>
-                <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </template>
-        </v-list>
-        <v-list two-line>
-          <template v-for="(item, index) in arrBox">
-            <!-- <v-subheader v-if="typeof item === 'number'" :key="item">{{index+1}} is loading</v-subheader>
-            <v-subheader v-else :key="item.id">
-              <a :href="item.url" target="_blank">
-                BY:{{item.by}}:
-                <p>{{ item.title }}</p>
-              </a>
-            </v-subheader>-->
-            <v-list-tile v-if="typeof item === 'number'" :key="item">{{index+1}} is loading</v-list-tile>
-            <v-list-tile v-else :key="item.title" avatar @click="openWindow(item.url)">
-              <v-list-tile-avatar>
-                <img :src="'https://www.gravatar.com/avatar/'+getMd5(item.by)+'?d=identicon&s=180'" />
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title v-html="item.by"></v-list-tile-title>
-                <v-list-tile-sub-title v-html="item.title"></v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-divider inset :key="index"></v-divider>
-          </template>
-        </v-list>
-      </v-card>
+      <v-container id="scroll-target" class="scroll-y">
+        <v-card v-scroll="onScroll">
+          <v-list two-line>
+            <v-subheader>top500</v-subheader>
+            <template v-for="(item, index) in arrBox">
+              <v-list-tile v-if="typeof item === 'number'" :key="item">{{index+1}} is loading</v-list-tile>
+              <v-list-tile v-else :key="item.title" avatar @click="openWindow(item.url)">
+                <v-list-tile-avatar>
+                  <img
+                    :src="'https://www.gravatar.com/avatar/'+getMd5(item.by)+'?d=identicon&s=180'"
+                  />
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title v-html="item.by"></v-list-tile-title>
+                  <v-list-tile-sub-title v-html="item.title"></v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-divider inset :key="index"></v-divider>
+            </template>
+          </v-list>
+        </v-card>
+      </v-container>
     </v-flex>
   </v-layout>
 </template>
@@ -63,29 +43,7 @@ export default class TopList extends Mixins(MyMixin) {
 
   public arrBox: any[] = [];
 
-  public items: any[] = [
-    { header: "Today" },
-    {
-      avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
-      title: "Brunch this weekend?",
-      subtitle:
-        "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
-    },
-    { divider: true, inset: true },
-    {
-      avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
-      title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-      subtitle:
-        "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
-    },
-    { divider: true, inset: true },
-    {
-      avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
-      title: "Oui oui",
-      subtitle:
-        "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-    }
-  ];
+  public offsetTop: string | number = 0;
 
   created() {}
 
@@ -98,7 +56,7 @@ export default class TopList extends Mixins(MyMixin) {
   changeArr() {
     this.arrBox.forEach((re: number, index: number): void => {
       this.apis.items.itemget(re).then(res => {
-        // console.log(res);
+        console.log(res);
         this.$set(this.arrBox, index, res);
       });
     });
@@ -112,6 +70,13 @@ export default class TopList extends Mixins(MyMixin) {
 
   getMd5(str: string) {
     return md5(str);
+  }
+
+  onScroll(e: any) {
+    const el = e.target.scrollingElement
+    this.offsetTop = e.target.scrollTop;
+    console.log(e, el.clientHeight, el.scrollHeight , el.scrollTop);
+    
   }
 
   titleClick = (e: string): void => {

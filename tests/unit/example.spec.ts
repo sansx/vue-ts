4b3bea4a17fe4d460e85e8dcc680db97d007fab2
sansx <xiathxth@gmail.com>
2019-08-03 23:18:02 +0800

    :ambulance: reset lockfile

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,28 @@
-import { shallowMount } from "@vue/test-utils";
+import { createLocalVue, mount, shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import vuetify from "vuetify";
+import Vue from "vue";
 
 describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
+  let wrapper: any;
+  let title = "Weather App";
+  beforeEach(() => {
+    // const localVue = createLocalVue()
+    Vue.use(vuetify);
+
+    wrapper = shallowMount(HelloWorld);
+  });
+  it("has an h2", () => {
+    expect(wrapper.contains("p")).toBe(true);
   });
 });
+
+// describe("HelloWorld.vue", () => {
+// it("renders props.msg when passed", () => {
+// const msg = "new message";
+// const wrapper = shallowMount(HelloWorld, {
+// propsData: { msg }
+// });
+// expect(wrapper.text()).toMatch(msg);
+// });
+// });
